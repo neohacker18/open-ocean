30594d236385e7f9de23fd26af03810ccd04fb97
Saket Mundra <saketmundra2707@gmail.com>
2022-11-05 12:31:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Home from "./pages/home"
+import ProductList from "./pages/ProductList"; 
+import Product from "./pages/Product"; 
+import Register from "./pages/Register"; 
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+
+
+
+const App = () => {
+  return <Home/>
+};
+
+export default App;
